4bde136b007b5073b8af78e8d4574bd8ba19586f
Pol Llovet <pol.llovet@gmail.com>
2009-06-26 18:45:45 -0600

    greened the tests

--- pg1/passgen.rb
+++ pg1/passgen.rb
@@ -1,3 +1,14 @@
 class Passgen
+  def initialize(len,num)
+    @pass = []
+    num.times { @pass << createPass(len)}
+  end
   
+  def createPass(n)
+    Array.new(n)
+  end
+  
+  def to_a
+    @pass
+  end
 end
