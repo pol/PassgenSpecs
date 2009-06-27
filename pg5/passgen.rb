0218ac6e3105a996f62047a092e1765182dd6a94
Pol Llovet <pol.llovet@gmail.com>
2009-06-26 19:34:07 -0600

    greened the specs, and thought of something...

--- pg5/passgen.rb
+++ pg5/passgen.rb
@@ -1,3 +1,14 @@
 class Passgen
+  def initialize(num,len)
+    @num = Array.new(num).map { 11 }
+    @len = Array.new(len).map { 11 }
+  end
   
+  def gen_passwd
+    @len
+  end
+    
+  def gen_passwd_list
+    @num
+  end
 end
