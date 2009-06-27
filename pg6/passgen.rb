bc28975a44caa31951790d8023cb017d42fe52ac
Pol Llovet <pol.llovet@gmail.com>
2009-06-26 19:50:49 -0600

    greened pg6

--- pg6/passgen.rb
+++ pg6/passgen.rb
@@ -1,3 +1,17 @@
 class Passgen
   
+  def pw_characters(num)
+    chars = ('a'..'z').map{ |i| i } + ('A'..'Z').map{ |i| i } + (0..9).map{ |i| i.to_s }
+    chars[num]
+  end
+  
+  def num_pw_test
+    5
+  end
+  alias_method :get_length_test, :num_pw_test
+  alias_method :one_pw_test, :num_pw_test
+  
+  def create_pw_a
+    Array.new(num_pw_test)
+  end
 end
