c43d389940630f29d605ef6969aaee60a4285696
Pol Llovet <pol.llovet@gmail.com>
2009-06-26 19:18:06 -0600

    specs greened for pg4!  More fun on this one. :)

--- pg4/passgen.rb
+++ pg4/passgen.rb
@@ -1,3 +1,22 @@
 class Passgen
   
+  attr_accessor :passwords
+  
+  def initialize(num,len,str)
+    length(num)
+  end
+  
+  def length(len)
+    @passwords = Array.new(len).map { 1 }
+    @passwords.length
+  end
+  alias_method :numberOfPasswords, :length
+  
+  def chars(chars=nil)
+    @chars = ['a','b']
+    @chars
+  end
+  alias_method :availChars, :chars
+  alias_method :generate, :chars
+
 end
